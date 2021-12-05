438dd1f150cea3dc0e91dd3554f73fc660770fb5
alpha-omega-labs <88520218+alpha-omega-labs@users.noreply.github.com>
2021-12-05 00:04:34 -0500

    Delete 2_CryptoBirdies_migration.js

--- migrations/2_CryptoBirdies_migration.js
+++
@@ -1,9 +0,0 @@
-const ERC721 = artifacts.require("CryptoBirdies");
-
-module.exports = async function(deployer) {
-  await deployer.deploy(ERC721, "CryptoBird", "CBX");
-  const instance = await ERC721.deployed();
-  if(instance) {
-    console.log("CryptoBirdies successfully deployed.")
-  }
-}
